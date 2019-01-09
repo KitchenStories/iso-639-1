bb4ec2acdf14b7d3873b12446c643ef9a702900d
Oscar <oscar.braunert@kitchenstories.de>
2019-01-09 17:02:09 +0100

    create umd version

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,14 @@ import cleaner from 'rollup-plugin-cleaner'
 
 export default [{
   input: './src/index.js',
-  output: {
+  output: [{
     format: 'esm',
     file: './build/index.js',
-  },
+  }, {
+    format: 'umd',
+    file: './build/index.umd.js',
+    name: 'ISO6391'
+  }],
   plugins: [
     cleaner({
       targets: [
