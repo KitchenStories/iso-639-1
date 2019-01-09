98d52378a1b76da14636e777d520a82fb2fedbbd
Oscar <oscar.braunert@kitchenstories.de>
2019-01-09 15:54:55 +0100

    make methods public

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import LANGUAGES_LIST from './data';
 
 export default class ISO6391 {
-  static getLanguages(codes = []) {
+  getLanguages(codes = []) {
     return codes.map(code => ({
       code,
       name: ISO6391.getName(code),
@@ -9,23 +9,23 @@ export default class ISO6391 {
     }));
   }
 
-  static getName(code) {
+  getName(code) {
     return ISO6391.validate(code) ? LANGUAGES_LIST[code].name : '';
   }
 
-  static getAllNames() {
+  getAllNames() {
     return Object.values(LANGUAGES_LIST).map(l => l.name);
   }
 
-  static getNativeName(code) {
+  getNativeName(code) {
     return ISO6391.validate(code) ? LANGUAGES_LIST[code].nativeName : '';
   }
 
-  static getAllNativeNames() {
+  getAllNativeNames() {
     return Object.values(LANGUAGES_LIST).map(l => l.nativeName);
   }
 
-  static getCode(name) {
+  getCode(name) {
     const code = Object.keys(LANGUAGES_LIST).find(code => {
       const language = LANGUAGES_LIST[code];
 
@@ -37,11 +37,11 @@ export default class ISO6391 {
     return code || '';
   }
 
-  static getAllCodes() {
+  getAllCodes() {
     return Object.keys(LANGUAGES_LIST);
   }
 
-  static validate(code) {
+  validate(code) {
     return LANGUAGES_LIST[code] !== undefined;
   }
 }
